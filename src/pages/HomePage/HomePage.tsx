1b72ad6c43a3da2b04b4f84235ae37cc4a017343
kardashevsky <kardashevskydv@gmail.com>
2026-03-22 03:35:13 +0300

    added resume button

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import styles from "./HomePage.module.css"
 import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom"
 
 export default function HomePage() {
   const { t } = useTranslation()
@@ -11,6 +12,10 @@ export default function HomePage() {
           <span>{t("home.firstName")}</span>
           <span>{t("home.lastName")}</span>
         </h1>
+
+        <Link to="/resume" className={styles.button}>
+          {t("home.resumeButton")}
+        </Link>
       </div>
     </div>
   )
